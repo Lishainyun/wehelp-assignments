d8ebf9d6c1b1e570fef96e069af94717dd2fbac5
Lishainyun <95123882+Lishainyun@users.noreply.github.com>
2022-02-21 01:20:31 +0800

    Add files via upload

--- week_7/public/addFeatures.js
+++ week_7/public/addFeatures.js
@@ -1,39 +1,45 @@
 "use strict"
 
-let apiUrl = 'http://127.0.0.1:3000/api/members'
-let urlParams = new URLSearchParams(window.location.search)
+let seachBtn = document.getElementById('searchBtn');
 
-async function getData(url){
-    const response = await fetch(url, {
-        method: 'GET'
+seachBtn.addEventListener('click', () => {
+
+    let input = document.getElementById('searchName').value
+    let searchApiUrl = 'http://127.0.0.1:3000/api/members'
+
+    fetch(searchApiUrl + "?username=" + input, {
+        method:'GET'
     })
-    return await response.json()
-}
-
-getData(apiUrl + '?' + urlParams)
+    .then(response => response.json())
     .then((data) => {
 
         let resData = data["data"]
-        let formDiv = document.getElementById('searchForm')
-        let resDiv = document.createElement('div')
-
-        formDiv.appendChild(resDiv);
-        resDiv.setAttribute('style','text-align:center; padding: 5px; font-size:20px');
-        resDiv.style.display = 'block';
+        // let resDiv = document.createElement('div')
+        let resultDiv = document.getElementById('result')
+        
+        // resDiv.setAttribute('style','text-align:center; padding: 5px; font-size:20px');
+        // resDiv.style.display = 'block';
+        while (resultDiv.firstChild) {
+            resultDiv.removeChild(resultDiv.firstChild);
+        }
 
         if (resData !== null) {
             let resName = data["data"]["name"]
             let resMessage = document.createElement('p')
             let resTextNode = document.createTextNode(resName)
-            resDiv.appendChild(resMessage);
+            resultDiv.appendChild(resMessage);
             resMessage.appendChild(resTextNode)
             resMessage.setAttribute('style', 'font-size:20px')
-            resDiv.style.display = 'block';
-        }else if(urlParams === null) {
+            resultDiv.style.display = 'block';
+        }else{
+            let err = document.createElement('p')
             let errMessage = "查無資料";
             let errTextNode = document.createTextNode(errMessage);
-            resDiv.appendChild(errTextNode);
-            resDiv.style.display = 'block';
+            resultDiv.appendChild(err);
+            err.appendChild(errTextNode)
+            err.style.display = 'block';
         }
-
     })
+    
+
+})
